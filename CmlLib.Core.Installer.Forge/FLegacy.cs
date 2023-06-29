0603fa45531b93d56794a1dce0ad25c73584295c
TaigoStudio <126141594+TaigoStudio@users.noreply.github.com>
2023-06-29 11:46:27 +0600

    Clear code

---
+++ CmlLib.Core.Installer.Forge/FLegacy.cs
@@ -0,0 +1,91 @@
+﻿using CmlLib.Core.Downloader;
+using ICSharpCode.SharpZipLib.Zip;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmlLib.Core.Installer.Forge
+{
+
+    /* 1.7.10 - 1.11.2 */
+    public class FLegacy : ForgeLoader
+    {
+
+        private readonly MinecraftPath minecraftPath;
+        private readonly string JavaPath;
+        private CMLauncher launcher;
+        private readonly IDownloader downloader;
+        public event DownloadFileChangedHandler? FileChanged;
+        public event EventHandler<string>? InstallerOutput;
+
+        public FLegacy(MinecraftPath minecraftPath, string JavaPath, CMLauncher launcher, IDownloader downloader, DownloadFileChangedHandler? FileChanged, EventHandler<string>? InstallerOutput) : base(minecraftPath, JavaPath, launcher, downloader, FileChanged, InstallerOutput)
+        {
+            this.minecraftPath = minecraftPath;
+            this.JavaPath = JavaPath;
+            this.downloader = downloader;
+            this.launcher = launcher;
+            this.FileChanged = FileChanged;
+            this.InstallerOutput = InstallerOutput;
+        }
+
+        public async Task<string> Install(string mcVersion, string forgeVersion, bool AlwaysUpdate = false)
+        {
+            if (!AlwaysUpdate && Directory.Exists(Path.Combine(minecraftPath.Versions, GetLegacyFolderName(mcVersion, forgeVersion))))
+                return $"{GetLegacyFolderName(mcVersion, forgeVersion)}";
+
+            var version_jar = minecraftPath.GetVersionJarPath(mcVersion); // get vanilla jar file
+            var install_folder = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()); //create folder in temp
+            if (!File.Exists(version_jar))
+                await launcher.CheckAndDownloadAsync(launcher.GetVersion(mcVersion)); //install vanilla version
+
+            await Parser.DownloadFile(mcVersion, forgeVersion, install_folder); //download forge version
+            File.Copy(Path.Combine(install_folder, "installer.jar"), Path.Combine(install_folder, "version.zip"));
+            new FastZip().ExtractZip(Path.Combine(install_folder, "version.zip"), install_folder, null); //unzip version
+
+            var installer = JObject.Parse(File.ReadAllText(Path.Combine(install_folder, "install_profile.json")));
+            var version = installer["versionInfo"] as JObject;
+            var installerData = installer["data"] as JObject;
+            var mapData = installerData == null ? new Dictionary<string, string?>() : mapping(installerData, "client", version_jar, install_folder);
+            var version_name = version["id"].ToString();
+            var destPath = (installer["install"] as JObject)["path"]?.ToString();
+            var universalPath = (installer["install"] as JObject)["filePath"]?.ToString();
+
+            if (string.IsNullOrEmpty(universalPath)) throw new InvalidOperationException("filePath property in installer was null");
+            if (string.IsNullOrEmpty(destPath)) throw new InvalidOperationException("path property in installer was null");
+
+            extractUniversal(install_folder, universalPath, destPath); // old installer
+            await checkLibraries(installer["libraries"] as JArray); //install libs
+            process(installer["processors"] as JArray, mapData, install_folder);
+            setupFolderLegacy(mcVersion, forgeVersion, install_folder, version_name, version.ToString()); //copy version.json and forge.jar
+
+            //########################AD URL##############################
+            Process.Start(Parser.getAdUrl()); //We support Forge developers!
+            //########################AD URL##############################
+
+            await launcher.GetAllVersionsAsync(); //update version list
+            return version_name;
+        }
+
+        private void setupFolderLegacy(string mcVersion, string forgeVersion, string install_folder, string version_name, string JVersion)
+        {
+            string version_folder = Path.Combine(minecraftPath.Versions, version_name);
+            var universal_jar = Convert.ToInt32(mcVersion.Split('.')[1]) < 10 ?
+                $"{GetLegacyForgeName(mcVersion, forgeVersion)}-universal.jar" :
+                $"{GetOldForgeName(mcVersion, forgeVersion)}-universal.jar";
+
+            if (Directory.Exists(version_folder))
+                Directory.Delete(version_folder, true); //remove version folder
+            Directory.CreateDirectory(version_folder); //create version folder
+
+            File.Copy(Path.Combine(install_folder, universal_jar),
+                Path.Combine(version_folder, $"{version_name}.jar")); //copy jar file
+            File.WriteAllText(Path.Combine(version_folder, $"{version_name}.json"), JVersion); //write version.json
+            Directory.Delete(install_folder, true); //remove temp folder
+        }
+
+    }
+}
---
+++ CmlLib.Core.Installer.Forge/Parser.cs
@@ -0,0 +1,61 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmlLib.Core.Installer.Forge
+{
+    public class Parser
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public static async Task<string> getForgeUrl(string mcVersion, string forgeVersion)
+        {
+            var document = new HtmlDocument();
+            var html = await httpClient.GetStringAsync($"https://files.minecraftforge.net/net/minecraftforge/forge/index_{mcVersion}.html");
+            document.LoadHtml(html);
+            var rows = document.DocumentNode.SelectNodes("//html[1]//body[1]//main[1]//div[2]//div[2]//div[2]//table[1]//tbody[1]//tr").ToList();
+            foreach (var row in rows)
+            {
+                var current_version = ClearName(row.Descendants(0).Where(n => n.HasClass("download-version")).FirstOrDefault().FirstChild.OuterHtml.Replace(" ", ""));
+                if (current_version == forgeVersion)
+                    return GetQueryString(row.ChildNodes[5].ChildNodes[1].ChildNodes[3].ChildNodes[3].Attributes["href"].Value, "url=");
+            }
+            throw new Exception("The version was not found on the official website");
+        }
+
+        public static string getAdUrl() =>
+            $"https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/";
+
+        private static string ClearName(string name) => name.Replace(" ", "").Replace("\n", "");
+
+        private static string GetQueryString(string url, string key)
+        {
+            int index = url.IndexOf('?');
+            var query = url.Substring(index + 1).Split('&').SingleOrDefault(s => s.StartsWith(key));
+            return query == null ? url : query.Replace(key, null);
+        }
+
+        public static async Task DownloadFile(string mcVersion, string forgeVersion, string install_folder)
+        {
+            Directory.CreateDirectory(install_folder);
+            var fileUrl = await getForgeUrl(mcVersion, forgeVersion);
+            var httpResult = await httpClient.GetAsync(fileUrl);
+            using var resultStream = await httpResult.Content.ReadAsStreamAsync();
+            using var fileStream = File.Create(Path.Combine(install_folder, "installer.jar"));
+            resultStream.CopyTo(fileStream);
+        }
+
+        public static string CalculateMD5(string filename)
+        {
+            using var md5 = MD5.Create();
+            using var stream = File.OpenRead(filename);
+            var hash = md5.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
